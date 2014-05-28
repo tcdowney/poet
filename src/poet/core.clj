c241a3387946e48ddb4cc630b78ea66c667805e7
Tim Downey <timcdowney@gmail.com>
2014-05-27 22:30:03 -0500

    Added a splash of literary eminence

--- src/poet/core.clj
+++ src/poet/core.clj
@@ -1,15 +1,14 @@
 (ns poet.core
   (:require [poet.nlp :as nlp])
+  (:require [poet.literary.eminence :as eminence])
   (:require [clojure.string]))
-
-(nlp/de-tokenize (nlp/tokenize (slurp "/home/tcdowney/Dev/spams/hey")))
 
 (defn reversify [tokens]
   (map clojure.string/reverse tokens))
 
 (defn poemify [path]
   (spit (str path "-poemified")
-        (nlp/de-tokenize (reversify (nlp/tokenize (slurp path))))))
+        (nlp/de-tokenize (eminence/solo-circumnavigation (nlp/tokenize (slurp path))))))
 
 (defn -main [& args]
   (when-not (empty? args)
