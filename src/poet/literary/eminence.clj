3a14f33c4d8512b693d054443b292767f647dee1
Tim Downey <timcdowney@gmail.com>
2014-06-03 19:41:24 -0500

    Added functions for the metathesis of ike into eik and iek

--- src/poet/literary/eminence.clj
+++ src/poet/literary/eminence.clj
@@ -12,3 +12,25 @@
     (clojure.string/replace text
                             #"\A[tT][hH][eE]\z"
                             #(str (first %1) (second (rest %1)) (second %1))))
+
+(defn
+  ike-metathesis-ie-variation
+  "Swaps all occurrences of the letters 'ike' with 'iek'"
+  [text]
+    (clojure.string/replace text
+                            #"\w[iI][kK][eE]"
+                            #(str (first %1 )
+                                  (second %1)
+                                  (second (rest (rest %1)))
+                                  (second (rest %1)))))
+
+(defn
+  ike-metathesis-ei-variation
+  "Swaps all occurrences of the letters 'ike' with 'iek'"
+  [text]
+    (clojure.string/replace text
+                            #"\w[iI][kK][eE]"
+                            #(str (first %1 )
+                                  (second (rest (rest %1)))
+                                  (second %1)
+                                  (second (rest %1)))))
--- test/poet/literary/eminence_test.clj
+++ test/poet/literary/eminence_test.clj
@@ -12,3 +12,27 @@
   (testing "that it handles mixed cases of 'the' and preserves the case of each letter"
     (is (= "teH" (the-metathesis "tHe")))
     (is (= "TEh" (the-metathesis "ThE")))))
+
+(deftest ike-metathesis-ie-variation-test
+  (testing "that it converts -ike and -ike- to iek"
+    (is (= "Did you liek your experience at IKEA?"
+           (ike-metathesis-ie-variation "Did you like your experience at IKEA?")))
+    (is (= "Siekston, Missouri" (ike-metathesis-ie-variation "Sikeston, Missouri"))))
+  (testing "that it does not convert ike- to iek"
+    (is (= "IKEA?" (ike-metathesis-ie-variation "IKEA?")))
+    (is (= "Ike" (ike-metathesis-ie-variation "Ike"))))
+  (testing "that it handles mixed cases of -'ike' and preserves the case of each letter"
+    (is (= "lieK" (ike-metathesis-ie-variation "liKe")))
+    (is (= "lIEk" (ike-metathesis-ie-variation "lIkE")))))
+
+(deftest ike-metathesis-ei-variation-test
+  (testing "that it converts -ike and -ike- to eik"
+    (is (= "Did you leik your experience at IKEA?"
+           (ike-metathesis-ei-variation "Did you like your experience at IKEA?")))
+    (is (= "Seikston, Missouri" (ike-metathesis-ei-variation "Sikeston, Missouri"))))
+  (testing "that it does not convert ike- to eik"
+    (is (= "IKEA?" (ike-metathesis-ei-variation "IKEA?")))
+    (is (= "Ike" (ike-metathesis-ei-variation "Ike"))))
+  (testing "that it handles mixed cases of -'ike' and preserves the case of each letter"
+    (is (= "leiK" (ike-metathesis-ei-variation "liKe")))
+    (is (= "lEIk" (ike-metathesis-ei-variation "lIkE")))))
