f98bc9333d54d14c0eb8b21ecfaf66fd46d95270
Tim Downey <timcdowney@gmail.com>
2014-06-03 19:12:01 -0500

    Added AOT

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojure-opennlp "0.3.2"]]
-  :main poet.core)
+  :main poet.core
+  :aot [poet.core])
